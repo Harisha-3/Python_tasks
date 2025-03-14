ee9d93e4ebcd519380d252b4ff77ee0aa152d426
EC2 Default User <ec2-user@ip-172-31-35-123.ap-south-1.compute.internal>
2025-03-14 05:25:59 +0000

    Added via git

---
+++ calc.py
@@ -0,0 +1,19 @@
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+ch = int(input("Enter your choice:"))
+while(ch != 0):
+
+                if(ch==1):
+                        print(num1," ","+"," ",num2," ","="," ",num1+num2)
+                if(ch==2):
+                        print(num1," ","-"," ",num2," ","="," ",num1-num2)
+                if(ch==3):
+                        print(num1," ","*"," ",num2," ","="," ",num1*num2)
+                if(ch==4):
+                        if(num2!=0):
+                                print(num1," ","/"," ",num2," ","="," ",num1/num2)
+                        else:
+                                print("Cannot be divisible")
+                print("\n        ******        \n")
+
+                ch = int(input("Enter\n 0 -> exit\n 1 -> Add\n 2 -> Subtract\n 3 -> Multiply\n 4 -> Divide\n >>"))
