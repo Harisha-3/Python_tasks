24c982391c985c10a6618f4aceb75f49c83332c5
EC2 Default User <ec2-user@ip-172-31-35-123.ap-south-1.compute.internal>
2025-03-14 05:45:42 +0000

    Deleted can't be divisible

--- calc.py
+++ calc.py
@@ -12,8 +12,7 @@ while(ch != 0):
                 if(ch==4):
                         if(num2!=0):
                                 print(num1," ","/"," ",num2," ","="," ",num1/num2)
-                        else:
-                                print("Cannot be divisible")
+                        
                 print("\n        ******        \n")
 
                 ch = int(input("Enter\n 0 -> exit\n 1 -> Add\n 2 -> Subtract\n 3 -> Multiply\n 4 -> Divide\n >>"))
